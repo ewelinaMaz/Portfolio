1d1caac21dcaad1de33b28081419b5a6d5ee507b
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-09-22 20:47:51 +0200

    Add Typical react and move images to public

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import image from '../../image/DSC_2181.JPG';
+//import image from;
 import clsx from 'clsx';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
-import JSicon from '../../image/JS.png';
+//import JSicon from ;
+//import ReactIcon from;
+//import NodeIcon from;
 import Link from '@material-ui/core/Link';
+import Typical from 'react-typical';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
@@ -16,28 +19,41 @@ const Component = ({className, children}) => (
   <div className={clsx(className, styles.root)}>
     <Container className={styles.Container}>
       <Grid item xs={12} sm={6} className={styles.Grid}>
-        <img src={image} alt="Ewelina" className={styles.image}></img>
+        <img src='/image/DSC_2181.JPG' alt="Ewelina" className={styles.image}></img>
       </Grid>
       <Grid item xs={12} sm={6}>
-        <h2 className={styles.name}>Front-end Developer</h2>
         <h2 className={styles.name}>Ewelina Mazurek</h2>
+        <Typical
+          className={styles.name}
+          loop={Infinity}
+          wrapper="h2"
+          steps={[
+            'Front-end Developer', 
+            6000,
+            'Engineer', 
+            3000,
+            'Writer',
+            2000,
+          ]}
+        >
+        </Typical>
       </Grid>
-      <Grid item md={12}>
+      <Grid item xs={12}>
         <h1>Skills</h1>  
       </Grid>
-      <Grid item xs={12} sm={4}>
-        <Button className={styles.button} component={Link}>
-          <img src={JSicon} alt='JS'/>
+      <Grid item xs={12} sm={6} md={4}>
+        <Button className={styles.button} component={Link} href="https://immense-shelf-23632.herokuapp.com/#/home">
+          <img src='/image/JS.png' alt='JS'/>
         </Button>
       </Grid>
-      <Grid item xs={12} sm={4}>
-        <Button className={styles.button} component={Link}>
-          <img src={JSicon} alt='JS'/>
+      <Grid item xs={12} sm={6} md={4}>
+        <Button className={styles.button} component={Link} href="https://sleepy-forest-92591.herokuapp.com/">
+          <img src='/image/React.png' alt='React'/>
         </Button>
       </Grid>
-      <Grid item xs={12} sm={4}>
+      <Grid item xs={12} sm={6} md={4}>
         <Button className={styles.button} component={Link}>
-          <img src={JSicon} alt='JS'/>
+          <img src='/image/node.png' alt='JS' className={styles.picture}/>
         </Button>
       </Grid>
     </Container>
