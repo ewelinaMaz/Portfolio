749d66895c538aee70f06f8dac216f28bee97e2b
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-09-26 20:54:16 +0200

    fix carrer test

--- src/components/common/Carrer/Carrer.js
+++ src/components/common/Carrer/Carrer.js
@@ -7,50 +7,46 @@ import { carrer } from '../../../redux/carrerRedux';
 
 import styles from './Carrer.module.scss';
 
-class Component extends React.Component {
-  render() {
-    
-    const {carrers, className} = this.props;
-    return (
-      <div className={clsx(className, styles.root)}>
-        <div className={styles.carrer}>
-          <Grid item xs={12} sm={6} className={styles.carrerGrid}>
-            <img src={carrers[0].photo} alt={carrers[0].proffesion} className={styles.carrerPhoto}/>
-          </Grid>
-          <Grid item xs={12} sm={6} className={styles.carrerGrid}>
-            <h2 className={styles.carrerTitle}>{carrers[0].proffesion}</h2>
-            <p className={styles.carrerText}>{carrers[0].description}</p>
-          </Grid>
-        </div>
-        <div className={styles.carrer}>
-          <Grid item xs={12} sm={6} className={styles.carrerGrid}>
-            <h2 className={styles.carrerTitle}>{carrers[1].proffesion}</h2>
-            <p className={styles.carrerText}>{carrers[1].description}</p>
-          </Grid>
-          <Grid item xs={12} sm={6} className={styles.carrerGrid}>
-            <img src={carrers[1].photo} alt={carrers[1].proffesion} className={styles.carrerPhoto}/>
-          </Grid>
-        </div>
-        <div className={styles.carrer}>
-          <Grid item xs={12} sm={6} className={styles.carrerGrid}>
-            <img src={carrers[2].photo} alt={carrers[2].proffesion} className={styles.carrerPhoto}/>
-          </Grid>
-          <Grid item xs={12} sm={6} className={styles.carrerGrid}>
-            <h2 className={styles.carrerTitle}>{carrers[2].proffesion}</h2>
-            <p className={styles.carrerText}>{carrers[2].description}</p>
-          </Grid>
-        </div>
-      </div>
-    );
-  }
-}
+const Component = ({ className, carrer}) => (
+
+  <div className={clsx(className, styles.root)}>
+    <div className={styles.carrer}>
+      <Grid item xs={12} sm={6} className={styles.carrerGrid}>
+        <img src={carrer[0].photo} alt={carrer[0].proffesion} className={styles.carrerPhoto}/>
+      </Grid>
+      <Grid item xs={12} sm={6} className={styles.carrerGrid}>
+        <h2 className={styles.carrerTitle}>{carrer[0].proffesion}</h2>
+        <p className={styles.carrerText}>{carrer[0].description}</p>
+      </Grid>
+    </div>
+    <div className={styles.carrer}>
+      <Grid item xs={12} sm={6} className={styles.carrerGrid}>
+        <h2 className={styles.carrerTitle}>{carrer[1].proffesion}</h2>
+        <p className={styles.carrerText}>{carrer[1].description}</p>
+      </Grid>
+      <Grid item xs={12} sm={6} className={styles.carrerGrid}>
+        <img src={carrer[1].photo} alt={carrer[1].proffesion} className={styles.carrerPhoto}/>
+      </Grid>
+    </div>
+    <div className={styles.carrer}>
+      <Grid item xs={12} sm={6} className={styles.carrerGrid}>
+        <img src={carrer[2].photo} alt={carrer[2].proffesion} className={styles.carrerPhoto}/>
+      </Grid>
+      <Grid item xs={12} sm={6} className={styles.carrerGrid}>
+        <h2 className={styles.carrerTitle}>{carrer[2].proffesion}</h2>
+        <p className={styles.carrerText}>{carrer[2].description}</p>
+      </Grid>
+    </div>
+  </div>
+);
+
 Component.propTypes = {
-  carrers: PropTypes.array,
+  carrer: PropTypes.array,
   className: PropTypes.string,
 };
 
 const mapStateToProps = state => ({
-  carrers: carrer(state),
+  carrer: carrer(state),
 });
 
 // const mapDispatchToProps = dispatch => ({
--- src/components/common/Carrer/Carrer.test.js
+++ src/components/common/Carrer/Carrer.test.js
@@ -3,10 +3,22 @@ import { shallow } from 'enzyme';
 import { CarrerComponent } from './Carrer';
 
 const mockProps = {
-  carrers: [
+  carrer: [
     {
       description: 'lorem ipsum',
       id: '1a',
+      photo: 'https://i.postimg.cc/ZKBrCG1X/automobile-4228723-1280.jpg',
+      proffesion: 'lorem ipsum',
+    },
+    {
+      description: 'lorem ipsum',
+      id: '1b',
+      photo: 'https://i.postimg.cc/ZKBrCG1X/automobile-4228723-1280.jpg',
+      proffesion: 'lorem ipsum',
+    },
+    {
+      description: 'lorem ipsum',
+      id: '1c',
       photo: 'https://i.postimg.cc/ZKBrCG1X/automobile-4228723-1280.jpg',
       proffesion: 'lorem ipsum',
     },
